61dd6b4641a500ea70e0e3d8cc31018918c9c383
artem <artem@irdo.pw>
2024-12-06 03:39:23 +0300

    docs: update README and App.tsx for JWT and host configuration
    - Revised README to clarify JWT usage for custom backend authorization and updated the description for the `host` parameter.
    - Modified `App.tsx` to replace hardcoded JWT token with a placeholder for better security practices.

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -7,7 +7,7 @@ import { AddressSuggestions } from '../../react-dadata/src/AddressSuggestions';
 // for dadata server use token and no host
 // for local/your backend server use jwt and host
 const DADATA_TOKEN = 'some_token';
-const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QGlyZG8ucHciLCJjcm1faWQiOiI1YzQ5YWE5MS03MTcwLTRjOTgtYmYxMy02YzcyYTliYWVmYzUiLCJyb2xlIjoib3duZXIiLCJleHAiOjE3MzM0NDY3NTR9.ILbt6PSfem6OHHveP8wDMExTlZl0BsfIHQXphXLKIvo';
+const JWT_TOKEN = 'some_jwt';
 
 function App() {
   if (!JWT_TOKEN) {
